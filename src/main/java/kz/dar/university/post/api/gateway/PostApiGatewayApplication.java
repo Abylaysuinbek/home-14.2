7e6cf7d9ff2ac0414adcaef03d29e0f67c1a7d3a
Abylay <s.a.b02@mail.ru>
2023-08-27 16:02:58 +0600

    Initial master

---
+++ src/main/java/kz/dar/university/post/api/gateway/PostApiGatewayApplication.java
@@ -0,0 +1,11 @@
+package kz.dar.university.post.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class PostApiGatewayApplication {
+
+}
